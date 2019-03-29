8393571ff09eedbfda88108360136cd5d6e7579f
hheiling <hheiling@live.unc.edu>
2019-03-29 10:19:38 -0400

    Outlined some initial first steps

--- Lasso Rough Draft - Hillary.R
+++ Lasso Rough Draft - Hillary.R
@@ -11,6 +11,38 @@ penal = function(y, X, lam, B, family = "binomial"){
   # lam = penalization parameter
   # B = previous iteration value of beta vector
   
+  # Set current iteration B0 equal to imported B
+  B0 = B
   
+  # Define working response, weights, and current-iteration residuals
+  # ...
+  
+  # Define convergence criteria
+  eps = -Inf
+  tol = 10^-6
+  maxit = 500 # Do we need a max number of iterations? Perhaps just until we know the code works.
+  iter = 0
+  
+  while(eps > tol & iter < maxit)
+  for(j in 1:ncol(X)){
+    
+  }
+  
+  # Return updated B and convergence criteria for each lambda
+  return(list(lambda = lam, B_new = B, crit = BIC))
   
 }
+
+# Find lambda_max and lambda_min
+lam = seq(from = lambda_max, to = lambda_min, by = -0.01)
+# For lambda_max, set initial value of B = vector of 0s
+B = numeric(ncol(X))
+results = list()
+
+for(l in 2:length(lam)){
+    results[l] = penal(y, X, lam[l], B, family = "binomial") # results[[l]] ?
+    B = results$B_new
+  
+}
+
+# In final results list, find B and lambda combo with the lowest BIC
