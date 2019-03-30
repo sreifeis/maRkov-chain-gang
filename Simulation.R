023a87e83790e9b19fc61f2871e1b82fdd68d8fa
hheiling <hheiling@live.unc.edu>
2019-03-30 11:50:32 -0400

    Start of set-up for .cpp file

--- Simulation.R
+++ Simulation.R
@@ -36,82 +36,14 @@ log_lam = seq(from = log(lambda_max), to = log(lambda_min), by = -0.05)
 B = numeric(length = ncol(X))
 results = list()
 lambda = exp(log_lam)
+results[[1]] = list(`lambda` = lambda[1], B_new = B, crit = Inf)
 
 for(l in 2:length(lambda)){
+  # B = results[[l-1]]$B_new
   results[[l]] = penal(y, X, lambda[l], B, family = "binomial") 
   B = results[[l]]$B_new
-  
 }
 
 results[[length(lambda)]]
-results[[10]]
+results[[100]]
 
-########################################################
-# Testing penal function - issue with not updating B properly.
-
-lam = lambda[100]
-j = 2
-
-# calc function
-# Calculates updated B[j], used in "penal" function below
-
-# Define working response, weights, and current-iteration residuals
-eta = X %*% B # Linear predictor in terms of current values of B
-p = exp(eta) / (1 + exp(eta)) # pi for current values, used in weights and working reponse
-W = diag(as.numeric(p*(1-p))) # Make weights into diagonal matrix
-y_wr = (y - p) / (p * (1-p)) + X %*% B # Working response
-# res = solve(W) %*% (y - p) # residuals based on current values
-v_j = (1/nrow(X)) * t(X[,j]) %*% W %*% X[,j]
-# z_j = (1/nrow(X)) * t(X[,j]) %*% W %*% res + v_j * B[j]
-# Alternatively:
-z_j = (1/nrow(X)) * t(X[,j]) %*% W %*% (y_wr - X[,-j] %*% B[-j])
-
-# Use above values to solve for next iteration value of Bj
-if(z_j > 0 & lam < abs(z_j)){
-  b_j = (z_j - lam) / v_j
-}else if(z_j < 0 & lam < abs(z_j)){
-  b_j = (z_j + lam) / v_j
-}else{
-  b_j = 0
-}
-
-# penal function
-
-    # Define B0 = B from last iteration - use in convergence calculation
-    B0 = B
-    
-    for(j in 1:ncol(X)){
-      
-      if(iter == 0){ # Update all B[j]
-        b_j = calc(y, X, B, lam, j)
-        
-      }else{ # Only update non-zero B[j] after first round
-        
-        if(B[j] != 0){
-          b_j = calc(y, X, B, lam, j)
-        }else{
-          b_j = 0
-        }
-      }
-    
-      B[j] = b_j
-    }
-    
-    # Evaluate convergence criteria - Euclidean distance
-    eps = sqrt((B - B0)^2)
-    
-    # Update iterations
-    iter = iter + 1
-    if(iter == maxit){warning("Model failed to converge within maximum number of iterations")}
-    
-  
-  # Evaluate BIC for resulting model
-  # From library(stats4): BIC function OR
-  # BIC = -2 (log-lik) + n_param * log(n_obs)
-  p_vec = exp(X %*% B)/(1+exp(X %*% B))
-  BIC = -2 * sum( y * log(p_vec) + (1-y) * log(1-p_vec) ) + ncol(X) * log(nrow(X))
-  
-  # Return updated B and BIC criteria for each lambda
-  return(list(lambda = lam, B_new = B, crit = BIC))
-  
-}
---
+++ lasso.cpp
@@ -0,0 +1,35 @@
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+using namespace Rcpp;
+
+double update(arma::vec y, arma::mat X, arma::vec B, double lam, unsigned int j){
+  unsigned int n = X.n_rows;
+  unsigned int p = X.n_cols;
+  double b_j;
+  arma::vec prob(n);
+  unsigned int k = 0;
+  
+  arma::vec eta = X * B;
+  for(k = 0; k < n; k++){
+    prob[k] = exp(eta[k]) / (1 + exp(eta[k]));
+    }
+  arma::mat W = diagmat(p);
+  arma::vec res = solve(W) * (y - p) ;
+  
+  return b_j;
+}
+
+// [[Rcpp::export]]
+List penalize(NumericVector x) {
+  // return x * 2;
+}
+
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically 
+// run after the compilation.
+//
+
+/*** R
+# timesTwo(42)
+*/
